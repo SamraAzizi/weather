33b8ce638bb9ea41f8ff4cdd79b4a8c0aab573df
SamraAzizi <samarraazizi@gmail.com>
2024-04-21 12:58:32 +0500

    my commit

--- weather/src/components/search/index.jsx
+++ weather/src/components/search/index.jsx
@@ -1,8 +1,20 @@
 
 
 
-export default function Search(){
-    return <div>
-        Search
+export default function Search({search, setSearch}){
+    return <div className="search-engine">
+
+        <input
+        type="text"
+        className="city-search"
+        placeholder="Enter City Name"
+        name="search"
+        value={search}
+        onChange={(event)=>setSearch(event.target.value)}
+        
+        
+        />
+        <button></button>
+       
     </div>
 }
