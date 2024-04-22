2ec3a8d4a7e313d665b560936f67d84e4a1b1d61
SamraAzizi <fahimaazizi369@gmail.com>
2024-04-22 12:47:07 +0500

    my commit

--- weather/src/components/weather/index.jsx
+++ weather/src/components/weather/index.jsx
@@ -1,10 +1,23 @@
 
-
+import { useState } from "react"
+import Search from "../search"
 
 
 export default function Weather(){
+    const [search, setSearch] = useState('');
+
+
+    async function handleSearch(){
+
+    }
+
     return <div>
-        <Search/>
+        <Search
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+        
+        />
         Weather
     </div>
 }
